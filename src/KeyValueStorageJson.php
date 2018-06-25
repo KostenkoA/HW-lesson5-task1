cec0c3ee5d74b0bce577cb5f3da12dfe62775f7d
KostenkoA <kostenkoam1212@gmail.com>
2018-06-25 17:34:47 +0300

    Json realization with construct

--- src/KeyValueStorageJson.php
+++ src/KeyValueStorageJson.php
@@ -6,16 +6,22 @@ namespace App\Storage;
 class KeyValueStorageJson implements KeyValueStorageInterface
 {
     private $jsonStorage = [];
+    private $jsonPath;
+
+    public function __construct(string $jsonPath)
+    {
+        $this->jsonPath = $jsonPath;
+    }
 
     public function set(string $key, $value): void
     {
         $this->jsonStorage[$key] = $value;
-        \file_put_contents('./Storage/KeyValueStorage.json',\json_encode($this->jsonStorage));
+        \file_put_contents($this->jsonPath,\json_encode($this->jsonStorage));
     }
 
     public function get(string $key)
     {
-        $dataObject = \json_decode(\file_get_contents('./Storage/KeyValueStorage.json'));
+        $dataObject = \json_decode(\file_get_contents($this->jsonPath));
         if (isset($dataObject->{$key})){
             return $dataObject->{$key};
         }
@@ -23,7 +29,7 @@ class KeyValueStorageJson implements KeyValueStorageInterface
 
     public function has(string $key): bool
     {
-        $dataObject = \json_decode(\file_get_contents('./Storage/KeyValueStorage.json'));
+        $dataObject = \json_decode(\file_get_contents($this->jsonPath));
         if (isset($dataObject->{$key})){
             return true;
         }
@@ -33,16 +39,16 @@ class KeyValueStorageJson implements KeyValueStorageInterface
 
     public function remove(string $key): void
     {
-        $dataObject = \json_decode(\file_get_contents('./Storage/KeyValueStorage.json'));
+        $dataObject = \json_decode(\file_get_contents($this->jsonPath));
         if (isset($dataObject->{$key})){
             unset($dataObject->{$key});
-            \file_put_contents('./Storage/KeyValueStorage.json',\json_encode($dataObject));
+            \file_put_contents($this->jsonPath,\json_encode($dataObject));
         }
 
     }
 
     public function clear(): void
     {
-        \file_put_contents('./Storage/KeyValueStorage.json',\json_encode($this->jsonStorage = []));
+        \file_put_contents($this->jsonPath,\json_encode($this->jsonStorage = []));
     }
 }
